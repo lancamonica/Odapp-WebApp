dd6bf643f4c71a557cd0c46eafd4cbd8c4b6818e
unknown <mo_silva1015@icloud.com>
2020-03-06 20:46:29 -0300

    feat: make container and styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
+import PatientsItem from "../src/components/PatientsItem";
+import PatientsForm from "../src/components/PatientsForm";
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
+import Header from "../src/components/Header";
+import Search from "./components/Search";
 
 function App() {
+  const [patients, setPatients] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [clearSearch, setClearSearch] = useState(false);
+  const [edit, setEdit] = useState(false);
+  const [editPatient, setEditPatient] = useState({
+    name: "",
+    age: "",
+    city: "",
+    state: "",
+    _id: null
+  });
+
+  async function loadPatients() {
+    const response = await api.get("/patients");
+    setPatients(response.data);
+  }
+
+  useEffect(() => {
+    loadPatients();
+  }, []);
+
+  async function handleAddPatients(data) {
+    const response = await api.post("/patients", data);
+    setPatients([...patients, response.data]);
+  }
+
+  async function handleDeletePatient(patientId) {
+    await api.delete(`/patient/${patientId}`);
+    const index = patients.findIndex(patient => patient._id === patientId);
+    patients.splice(index, 1);
+    setPatients([...patients]);
+    alert("Paciente excluido com sucesso");
+    await loadPatients();
+  }
+
+  async function onClickUpdatePatient(patient) {
+    setEdit(true);
+    setEditPatient(patient);
+  }
+
+  async function handleUpdatePatient(patient) {
+    await api.put(`/patient/${patient.id}`, {
+      name: patient.name,
+      age: patient.age,
+      city: patient.city,
+      state: patient.state
+    });
+    alert("Paciente atualizado com sucesso!.");
+
+    await loadPatients();
+    setEdit(false);
+    setEditPatient({
+      name: "",
+      age: "",
+      city: "",
+      state: "",
+      _id: null
+    });
+  }
+
+  async function searchName() {
+    if (searchText !== "") {
+      const patient = patients.find(
+        patient => patient.name === searchText.trim()
+      );
+      if (patient === undefined) {
+        alert("Paciente não encontrado.");
+        setSearchText("");
+      } else {
+        setPatients([patient]);
+        setClearSearch(true);
+      }
+    }
+  }
+
+  async function loadList() {
+    if (clearSearch && searchText === "") {
+      await loadPatients();
+      setClearSearch(false);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <div className="header">
+        <Header text="Odapp Challenge" />
+      </div>
+      <aside>
+        <PatientsForm
+          onSubmitForm={edit ? handleUpdatePatient : handleAddPatients}
+          edit={edit}
+          editPatient={editPatient}
+        />
+      </aside>
+      <main>
+        <Search
+          text={searchText}
+          onChangeSearchText={e => setSearchText(e.target.value)}
+          onClickSearch={clearSearch ? loadList : searchName}
+        />
+        <ul>
+          {patients.map(patient => (
+            <PatientsItem
+              key={patient._id}
+              patient={patient}
+              onClickDelete={() => handleDeletePatient(patient._id)}
+              onClickUpdate={() => onClickUpdatePatient(patient)}
+            />
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
